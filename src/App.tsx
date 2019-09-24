caba1f7811a452c9396bb6f2153ebbfd1d2894e9
Raissa Ferreira <rai200890@gmail.com>
2019-09-24 01:13:42 -0300

    feature: Install react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>TODOs List</h2>;
+}
+
+function NewTODO() {
+  return <h2>Add TODO</h2>;
+}
+
+function AppRouter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todos/new">Add TODO</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Index} />
+        <Route path="/todos/new" component={NewTODO} />
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
